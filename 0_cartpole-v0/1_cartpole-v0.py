c6a77afd09a9daa4a77e604d4f8872c9e3b8081d
박경태 <gyeongtae.park@catenoid.net>
2022-04-20 20:54:33 +0900

    예제 확인 중

--- 0_cartpole-v0/1_cartpole-v0.py
+++ 0_cartpole-v0/1_cartpole-v0.py
@@ -1,17 +1,43 @@
+# python 3.7
+
+# 에피소드가 종료되는 조건
+# 막대기 각도 : -12도 ~ +12도
+# 카트의 위치 : -2.4 ~ +2.4
+# 시간 > 200
+
+# import gym
+
+# env = gym.make('CartPole-v0')
+
+# # 새로운 에피소드 호출
+# env.reset()
+
+# for _ in range(1000):
+
+#     # 행동 이전 관찰값
+#     env.render()
+
+#     # 행동 이후 관찰값
+#     # step의 반환 : observation, reward, done, info
+#     env.step(env.action_space.sample())
+    
+
+# env.close()
+
+# step 1
+# import gym
+
+# env = gym.make('CartPole-v0')
+# observation = env.reset()
+
+# print(observation)
+# # 카트의 위치, 카트의 속도, 막대기의 각도, 막대기의 회전율
+
+# step 2
 import gym
 
 env = gym.make('CartPole-v0')
+observation = env.reset()
+action = env.action_space.sample()
 
-for i_episode in range(20):
-    observation = env.reset()
-
-    for t in range(100):
-        env.render()
-        print(observation)
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
-env.close()
+print(action)
