bafb9261a1b661b071fa861425b2d03f5e11daf0
박경태 <gyeongtae.park@catenoid.net>
2022-04-21 17:47:40 +0900

    update custom-1

---
+++ 0_cartpole-v0/10_cartpole-v0-custom-1.py
@@ -0,0 +1,31 @@
+import gym
+import random
+
+env = gym.make('CartPole-v0')
+
+max_time_step = 100
+
+observation = env.reset()
+
+for time_step in range(max_time_step):
+    
+    env.render()
+
+    if observation[2] > 0:
+        action = 1
+    
+    elif observation[2] < 0:
+        action = 0
+    
+    else:
+        action = random.randrange(0, 2)
+
+    observation, reward, done, info = env.step(action)
+    print(observation, done)
+
+    if done:
+        
+        print(time_step + 1)
+        break
+
+env.close()
