66647c48c0910d8f7d6d0e46037c06da642ac575
박경태 <gyeongtae.park@catenoid.net>
2022-04-21 10:32:57 +0900

    코드에 대한 설명 추가 및 example, observation, action 수정

---
+++ 0_cartpole-v0/0_cartpole-v0-example.py
@@ -0,0 +1,41 @@
+import gym
+
+env = gym.make('CartPole-v0')
+
+env.reset()
+
+for i in range(500):
+
+    env.render()
+    env.step(env.action_space.sample())
+
+env.close()
+
+'''
+
+< 코드 설명 >
+
+import gym
+env = gym.make('CartPole-v0')
+
+# 새로운 에피소드를 시작
+env.reset()
+
+# 500 이라는 시간 동안 (약 10초 정도)
+for i in range(500):
+
+    # 행동 이전 관찰값
+    env.render()
+
+    # 행동 이후 관찰값
+    env.step(env.action_space.sample())
+
+# 환경 종료
+env.close()
+
+# 에피소드가 종료되는 조건
+# 막대기 각도 : -12도 ~ +12도
+# 카트의 위치 : -2.4 ~ +2.4
+# 시간 > 200
+
+'''
---
+++ 0_cartpole-v0/1_cartpole-v0-first-observation.py
@@ -0,0 +1,24 @@
+import gym
+
+env = gym.make('CartPole-v0')
+
+first_observation = env.reset()
+
+print('first_observation : ', first_observation)
+
+'''
+
+import gym
+
+env = gym.make('CartPole-v0')
+
+# 첫 관찰값
+first_observation = env.reset()
+
+print(first_observation)
+
+# 결과
+#  [카트의 위치, 카트의 속도, 막대기의 각도, 막대기의 회전율]
+# [0.00803868 -0.01096694 -0.02077952 -0.00761259]
+
+'''
