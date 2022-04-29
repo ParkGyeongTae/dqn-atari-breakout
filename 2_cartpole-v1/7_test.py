e30440f7fc1ca08497f81e11869095cb0ac43f9f
박경태 <gyeongtae.park@catenoid.net>
2022-04-30 00:43:10 +0900

    테스트코드 추가 모델 출력

--- 2_cartpole-v1/7_test.py
+++ 2_cartpole-v1/7_test.py
@@ -1,3 +1,5 @@
+################################################
+
 # import random
 
 # for _ in range(10):
@@ -6,22 +8,26 @@
 # for _ in range(10):
 #     print(_, random.random())
 
+################################################
 
-from keras.models                import Sequential
-from keras.layers                import Dense
-from tensorflow.keras.optimizers import Adam
+# from keras.models                import Sequential
+# from keras.layers                import Dense
+# from tensorflow.keras.optimizers import Adam
 
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# import os
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-def build_model():
-  model = Sequential()
-  model.add(Dense(128, input_dim = 4, activation = 'relu'))
-  model.add(Dense(64, activation = 'relu'))
-  model.add(Dense(32, activation = 'relu'))
-  model.add(Dense(16, activation = 'relu'))
-  model.add(Dense(2, activation = 'softmax'))
-  model.compile(loss='mse', optimizer = Adam())
-  return model
+# def build_model():
+#   model = Sequential()
+#   model.add(Dense(128, input_dim = 4, activation = 'relu'))
+#   model.add(Dense(64, activation = 'relu'))
+#   model.add(Dense(32, activation = 'relu'))
+#   model.add(Dense(16, activation = 'relu'))
+#   model.add(Dense(2, activation = 'softmax'))
+#   model.compile(loss='mse', optimizer = Adam())
+#   return model
 
-print(build_model())
+# model = build_model()
+# print(model.summary())
+
+################################################
